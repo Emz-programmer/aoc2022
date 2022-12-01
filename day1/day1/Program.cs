e543ad3d2ad7c0b010d55b02514648f99cf1366a
Emily <hynesemily99@gmail.com>
2022-12-01 17:14:45 +0000

    Day 1: Elves love their calorific intake

---
+++ day1/day1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = "../../../input.txt";
+            string[] input = File.ReadAllText(path).Split("\r\n\r\n");
+            int[] elfCalories = new int[input.Length];
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                int[] tempCalories = Array.ConvertAll(input[i].Split("\r\n"), int.Parse);
+                elfCalories[i] = tempCalories.Sum();
+            }
+
+            Array.Sort(elfCalories);
+            Array.Reverse(elfCalories);
+
+            Console.WriteLine("Biggest Value: " + elfCalories[0]);
+            Console.WriteLine("Sum of top 3: " + elfCalories.Take(3).Sum());
+        }
+    }
+}
