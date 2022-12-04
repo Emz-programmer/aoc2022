10559f0d7833b58646e8572b925c42ffd131280f
Emily <hynesemily99@gmail.com>
2022-12-04 14:28:19 +0000

    Now with more efficient maids

---
+++ day5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace day5
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = "../../../input.txt";
+            string[] input = File.ReadAllText(path).Split("\r\n");
+        }
+    }
+}
