0378b9ccfc6374a082ec1aa67ab6138118c7d2e5
Emily <hynesemily99@gmail.com>
2022-12-06 07:47:42 +0000

    Oh my, what a big package you have

--- README.md
+++ README.md
@@ -14,3 +14,4 @@ https://adventofcode.com/2022
 | Day 3   | The elves have massive loads                  |     2     |
 | Day 4   | All they needed was a maids dress...          |     2     |
 | Day 5   | Massive boxes for tiny elves    	          |     2     |
+| Day 6   | Oh My! What a big package you have	          |     2     |
---
+++ day6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace day6
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = "../../../input.txt";
+            string input = File.ReadAllText(path);
+
+            Console.WriteLine("Start of package: " + getMarker(input, 4));
+            Console.WriteLine("Start of message: " + getMarker(input, 14));
+        }
+
+        static int getMarker(string datastream, int bufferSize)
+        {
+            for (int i = 0; i < datastream.Length - bufferSize - 1; i++)
+            {
+                string buffer = datastream.Substring(i, bufferSize);
+                if (isAllUnique(buffer)) return i + bufferSize;
+            }
+            return 0;
+        }
+
+        static bool isAllUnique(string buffer)
+        {
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                if (buffer.Substring(i + 1).Contains(buffer[i])) return false;
+            }
+            return true;
+        }
+    }
+}
